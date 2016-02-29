94e9d709f1dac300f430ec9bfe33940121ebd5a3
Weslo <wesley.rockholz@gmail.com>
2016-02-29 10:41:41 -0500

    add test nav html

--- js/components/nav.js
+++ js/components/nav.js
@@ -15,5 +15,24 @@ nav.controller = function() {
 
 // Create the nav view.
 nav.view = function() {
-  return m("div", "Hello, world!");
+  return m("header", [
+    m("div", {class: "nav-header"}, [
+      m("div", {class: "nav-collapse"}, [
+        m("ul", [
+          m("li", [
+            m("a", "Home")
+          ]),
+          m("li", [
+            m("a", "Game")
+          ]),
+          m("li", [
+            m("a", "Forums")
+          ]),
+          m("li", [
+            m("a", "Shop")
+          ]),
+        ])
+      ])
+    ])
+  ]);
 }
