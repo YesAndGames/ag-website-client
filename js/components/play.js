ab1dc3ffd4c81f395a2d059b8d26b703b087b0a4
Weslo <wesley.rockholz@gmail.com>
2016-03-01 13:08:34 -0500

    open beta descriptions

--- js/components/play.js
+++ js/components/play.js
@@ -16,15 +16,24 @@ var betaSignup = {
   // Create the view.
   view: function() {
     return m("section", {class: "beta-signup hero"}, [
-      m("div", {class: "vertical-center"}, [
-        m("div", [
-          m("h3", "Sign up for"),
-          m("h1", "Open Beta"),
-          m("div", [
-            m("p", "Adventure Guild will eventually be publicly available via an Open Beta event before the official release of the game."
-            + "Open Beta will follow an Early Alpha Access event, but will be free to access on all available platforms."
-            + "Our testing events may have limited space, and participants will be chosen at random."
-            + "To register for our public events and get email notifications when we broadcast updates, sign up below.")
+      m("div", [
+        m("div", {class: "vertical-center"}, [
+          m("div", {class: "col span_4_of_12"}, [
+            m("h2", "Sign up for"),
+            m("h1", "Open Beta"),
+            m("div", [
+              m("p", "Adventure Guild will eventually be publicly available via an Open Beta event before the official release of the game. "
+              + "Open Beta will follow an Early Alpha Access event, but will be free to access on all available platforms. "
+              + "Our testing events may have limited space, and participants will be chosen at random. "
+              + "To register for our public events and get email notifications when we broadcast updates, sign up here. ")
+            ]),
+            m("div", {class: "platform-images"}, [
+              m("img", {src: "imgs/logos/ios.png", alt: "iOS"}),
+              m("img", {src: "imgs/logos/android.png", alt: "Android"}),
+              m("img", {src: "imgs/logos/windows.png", alt: "Windows 10 Mobile"})
+            ])
+          ]),
+          m("div", {class: "cold span_8_of_12"}, [
           ])
         ])
       ])
