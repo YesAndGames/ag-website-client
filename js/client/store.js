d3bf4778015ee3bee788c3516a3783031a99a52d
RickNabb <nrabb@outlook.com>
2016-03-10 16:16:03 -0500

    Updated store client functions

--- js/client/store.js
+++ js/client/store.js
@@ -82,7 +82,6 @@ function storeAddInventoryItem(name, description, price, imageURL, callback) {
  * @param {number}   itemId   The ID of the item to retrieve.
  * @param {function} callback Code to run with the response.
  */
-
 function storeGetItem(itemId, callback) {
   'use strict';
   var settings = {
@@ -104,22 +103,176 @@ function storeGetItem(itemId, callback) {
   });
 }
 
+/**
+ * Create an order for a specific user through the store.
+ * @author Nick Rabb <nrabb@outlook.com>
+ * @param {function} callback Code to run with the result of the request.
+ */
+
 function storeCreateOrder(callback) {
   'use strict';
+  // Make sure the user is logged in
+  if (dataCacheRetrieve(dataCacheAuthVar) !== 'undefined') {
+    var userId = dataCacheRetrieve(dataCacheAuthVar).id,
+      settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": yag_api_endpoint + "store/orders/",
+        "method": "POST",
+        "headers": {
+          "content-type": "application/json",
+          "cache-control": "no-cache"
+        },
+        "processData": false,
+        "data": {
+          "userId": userId
+        }
+      };
+
+    $.ajax(settings).done(function (response) {
+      if (response !== 'undefined') {
+        callback(response);
+      }
+    });
+  }
 }
 
+
+/**
+ * storeDeleteOrder - Delete an order that a user has for the store.
+ *
+ * @param  {number}   orderId  The id of the order to delete
+ * @param  {function} callback Code to run with the result
+ */
 function storeDeleteOrder(orderId, callback) {
   'use strict';
+  if (dataCacheRetrieve(dataCacheAuthVar) !== 'undefined') {
+    var userId = dataCacheRetrieve(dataCacheAuthVar).id,
+      settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": yag_api_endpoint + "store/orders/" + orderId,
+        "method": "DELETE",
+        "headers": {
+          "content-type": "application/json",
+          "cache-control": "no-cache"
+        },
+        "processData": false,
+        "data": {
+          "userId": userId
+        }
+      };
+
+    $.ajax(settings).done(function (response) {
+      if (response !== 'undefined') {
+        callback(response);
+      }
+    });
+  }
 }
 
+
+/**
+ * storeGetOrder - Get the details of an order.
+ *
+ * @param  {number}   orderId  The id of the order to retrieve.
+ * @param  {function} callback Code to run with the result.
+ */
 function storeGetOrder(orderId, callback) {
   'use strict';
+  if (dataCacheRetrieve(dataCacheAuthVar) !== 'undefined') {
+    var userId = dataCacheRetrieve(dataCacheAuthVar).id,
+      settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": yag_api_endpoint + "store/orders/" + orderId,
+        "method": "GET",
+        "headers": {
+          "content-type": "application/json",
+          "cache-control": "no-cache"
+        },
+        "processData": false,
+        "data": {
+          "userId": userId
+        }
+      };
+
+    $.ajax(settings).done(function (response) {
+      if (response !== 'undefined') {
+        callback(response);
+      }
+    });
+  }
 }
 
+
+/**
+ * storeAddItemToOrder - Add an item to a user's order.
+ *
+ * @param  {number}   orderId  The id of the order to add an item to.
+ * @param  {number}   itemId   The id of the item to add.
+ * @param  {function} callback Code to run with the result.
+ */
 function storeAddItemToOrder(orderId, itemId, callback) {
   'use strict';
+  if (dataCacheRetrieve(dataCacheAuthVar) !== 'undefined') {
+    var userId = dataCacheRetrieve(dataCacheAuthVar).id,
+      settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": yag_api_endpoint + "store/orders/" + orderId + "/items/",
+        "method": "POST",
+        "headers": {
+          "content-type": "application/json",
+          "cache-control": "no-cache"
+        },
+        "processData": false,
+        "data": {
+          "itemId": itemId,
+          "userId": userId
+        }
+      };
+
+    $.ajax(settings).done(function (response) {
+      if (response !== 'undefined') {
+        callback(response);
+      }
+    });
+  }
 }
 
-function storeRemoveItemFromOrder(orderId, itemId, callback) {
+
+/**
+ * storeRemoveItemFromOrder - Remove an item from a user's order.
+ *
+ * @param  {number}   orderId       The id of the order to remove an item from.
+ * @param  {number}   orderItemId   The id of the order item to remove.
+ * @param  {function} callback      Code to run with the result.
+ */
+function storeRemoveItemFromOrder(orderId, orderItemId, callback) {
   'use strict';
+  if (dataCacheRetrieve(dataCacheAuthVar) !== 'undefined') {
+    var userId = dataCacheRetrieve(dataCacheAuthVar).id,
+      settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": yag_api_endpoint + "store/orders/" + orderId + "/items/",
+        "method": "DELETE",
+        "headers": {
+          "content-type": "application/json",
+          "cache-control": "no-cache"
+        },
+        "processData": false,
+        "data": {
+          "orderItemId": orderItemId,
+          "userId": userId
+        }
+      };
+
+    $.ajax(settings).done(function (response) {
+      if (response !== 'undefined') {
+        callback(response);
+      }
+    });
+  }
 }
