998aacbda0f11cee5bf3f75598c0fb2081e1b237
Weslo <wesley.rockholz@gmail.com>
2016-03-10 11:54:46 -0500

    start press css with header

--- js/components/press.js
+++ js/components/press.js
@@ -1,7 +1,7 @@
 var presskit = {
   view: function() {
     return m("section", {class: "presskit"}, m("div", [
-      m(".press-header", m("img", {src: "imgs/press/header.png", alt: "Adventure Guild"})),
+      m(".press-header"),
       m("hr", {class: "hr-gradient"}),
       m("div", {class: "col span_4_of_12"}, m("div", [
 
