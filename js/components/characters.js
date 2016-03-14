02908a70dfa62fdddf1c280b2cb237763308bf65
Weslo <wesley.rockholz@gmail.com>
2016-03-14 14:58:16 -0400

    warning and proper sizing for characters player

--- js/components/characters.js
+++ js/components/characters.js
@@ -1,6 +1,23 @@
+var playerWarning = {
+  view: function() {
+    return m("div", m(".playerWarning", m("div", [
+        m("h2", "Create your Adventurer!"),
+        m(".playerWarningContent", [
+          m("p", "Try out our Adventurer creator through the experimental Unity WebGL player. "
+            + "Mobile browsers are unsupported and there may be issues with small screen sizes. "
+            + "If you find a bug, shoot us an email! We want to make the player as available as possible."),
+          m("br"),
+          m("strong", "Throughout this week, take a screenshot of your Adventurer and tweet it to @AdvGuildGame with the hashtag #AdventureGuildGDC for a chance to win a t-shirt!")
+        ])
+      ])),
+      m("hr", {class:"hr-gradient"})
+    );
+  }
+}
+
 var characterCreatorPlayer = {
   view: function() {
-    return m(".hero", m(".template", m("div", {class: "template-wrap clear"}, [
+    return m(".playerContainer", m(".template", m("div", {class: "embed-container clear"}, [
       m("canvas", {class: "emscripten", id: "canvas",
         oncontextmenu: "event.preventDefault()"}),
       m("script", {src: "js/plugins/UnityLoader.js"})
@@ -12,6 +29,7 @@ var characters = {
   view: function() {
     return [
       m.component(nav, {page: "adventurers"}),
+      m.component(playerWarning),
       m.component(characterCreatorPlayer),
       m.component(footer)
     ];
