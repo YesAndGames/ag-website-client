fb30b66260397bfa98e29d7792e8bc34e50cd457
Weslo <wesley.rockholz@gmail.com>
2016-03-31 11:54:14 -0400

    apparently added a new file full of modals

---
+++ js/components/modals.js
@@ -0,0 +1,17 @@
+// Modal that enables the user to change their password.
+var changePasswordModal = {
+  view: function() {
+    return m(".modal", {onclick: function (e) { closeModals (e); }},
+      m(".modalContent", {onclick: function (e) { e.stopPropagation (); }}, [
+        m("h2", "Change Password"),
+        m("p", "Change your Adventure Guild account password."),
+        m("div", m("form", {id: "changePasswordForm"}, [
+          m("input", {type: "password", name: "old-password", placeholder: "Old Password"}),
+          m("input", {type: "password", name: "new-password", placeholder: "New Password"}),
+          m("input", {type: "password", name: "confirm-new-password", placeholder: "Confirm New Password"}),
+          m("button", {type: "button", onclick: function (e) {}}, "Submit")
+        ]))
+      ])
+    );
+  }
+}
