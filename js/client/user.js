dbc7f270be85fc091d0d47d376589ee4ed0cd4bc
RickNabb <nrabb@outlook.com>
2016-03-31 16:28:10 -0400

    Added set email function to user client file

--- js/client/user.js
+++ js/client/user.js
@@ -33,3 +33,31 @@ function userGetData(userId, callback) {
     callback();
   });
 }
+
+/**
+ * Set a user's email address.
+ * @author Nick Rabb <nrabb@outlook.com>
+ * @param {number} userId     The ID of the user to fetch data for.
+ * @param {string} email      The email address to use for the user.
+ * @param {string} password   The user's password.
+ * @param {function} callback Code to run after the request comes back.
+ */
+function userSetEmail(userId, email, password, callback) {
+    'use strict';
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": yag_api_endpoint + "users/" + userId + "/setEmail",
+      "method": "POST",
+      "headers": {
+        "content-type": "application/json",
+        "cache-control": "no-cache"
+      },
+      "data": "{\n  \"email\": \"" + email + "\", \"password\":\" + password + \"\n}",
+      "processData": false
+    };
+
+    $.ajax(settings).done(function (response) {
+      callback();
+    });
+}
