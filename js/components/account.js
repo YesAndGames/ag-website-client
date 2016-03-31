0952cf6de5ad6dbc951bf29169083c5ab231f780
Weslo <wesley.rockholz@gmail.com>
2016-03-31 10:57:15 -0400

    show the user's kickstarter backer tier on their account page

--- js/components/account.js
+++ js/components/account.js
@@ -5,6 +5,7 @@ var account = {
   vm: {
     init: function() {
       account.user = m.prop(JSON.parse(dataCacheRetrieve(dataCacheAuthVar)));
+      console.log (account.user);
     }
   },
 
@@ -40,15 +41,70 @@ var accountDetails = {
       m("h1", account.user().username),
       m(".col span_8_of_12", [
         m("h3", "Account Details"),
+        m("h4", "Email Address"),
+        m.component(emailField),
+        m("h4", "Supporter Titles"),
+        m.component(kickstarterTitle),
       ]),
       m(".col span_4_of_12", [
         m("h3", "Manage Account"),
         m("ul", [
-          m("li", m("a", "Change Password")),
+          m("li", m("a", {onclick:function(e){openModal(e,changePasswordModal);}}, "Change Password")),
+          m("li", m("a", "Change Email Address")),
           m("li", m("a", "Redeem Key")),
           m("li", m("a", "View Transactions")),
         ]),
       ]),
     ]));
+  }
+}
+
+// Renders elements based on whether or not the user's email is found.
+var emailField = {
+  view: function() {
+    var email = account.user().email;
+    if (email != null && email != "") {
+      return m("p", email);
+    }
+    else {
+      return m("a", {onclick:function(e){openModal(e,missingEmailModal);}}, "Missing email, click to register!");
+    }
+  }
+}
+
+// Renders the user's Kickstarter title if they have one.
+var kickstarterTitle = {
+  view: function() {
+    var tier = account.user().tier;
+    if (tier > 0) {
+      return m("p", kickstarterTitle.getBackerTitle (tier));
+    }
+    else {
+      return null;
+    }
+  },
+
+  // returns the string representation of a backer tier
+  getBackerTitle: function(tier) {
+    switch(tier) {
+      case 1:
+        return "Bronze Adventurer";
+      case 2:
+        return "Silver Adventurer";
+      case 3:
+        return "Gold Adventurer";
+      case 4:
+        return "Platinum Adventurer";
+      case 5:
+        return "Renowned Adventurer";
+      case 6:
+        return "Heroic Adventurer";
+      case 7:
+        return "Epic Adventurer";
+      case 8:
+        return "Legendary Adventurer";
+      case 9:
+        return "Mythic Adventurer";
+    }
   }
 }
