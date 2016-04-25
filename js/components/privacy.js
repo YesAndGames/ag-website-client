bea15e393c03e4b5ff3526dfbb7640cf2b61b472
Weslo <wesley.rockholz@gmail.com>
2016-04-25 15:27:13 -0400

    render tos from parsed .md file

--- js/components/privacy.js
+++ js/components/privacy.js
@@ -1,3 +1,8 @@
+$.get("content/tos.md", function(data) {
+  tosPrivacyContent.tos(data);
+  m.redraw();
+}, 'text');
+
 var privacy = {
   view: function() {
     return [
@@ -9,13 +14,13 @@ var privacy = {
 }
 
 var tosPrivacyContent = {
+  tos: m.prop(""),
   view: function() {
     return m(".privacy-policy hero position-under-nav", m("div", [
       m("h2", "Terms of Service and Privacy Policy"),
       m("hr", {class: "hr-gradient"}),
       m("div", m.trust(marked(
-        "1. __Terms of Use:__ Welcome to Yes And Games ('__Yes And Games__') 'Adventure Guild' videogame ('__AG__'). These terms of use and privacy policy ('__Terms of Use__') apply to your use of the AG website ('__Website__') at www.adventureguildgame.com (or such other URL as the Website may subsequently be hosted at), the software, documentation and any associated materials comprising or made available to you in relation to AG (the '__Materials__') as well as any services offered or provided by Yes And Games or its nominated third parties in relation to AG (the '__Services__'). By making use of any of the Website, Materials and Services, you are deemed to have read and agree to be bound by these Terms of Use." +
-        "\n2. __Amendments:__ Yes And Games reserves the right to amend these Terms of Use from time to time upon placing any such amendments on the Website or by providing you direct notice of any such changes. Your continued use of any of the Website, Materials and Services thereafter will be deemed to be acceptance by you of any such changes to these Terms of Use."
+        tosPrivacyContent.tos()
         ), {
           gfm: true,
           smartypants: true,
