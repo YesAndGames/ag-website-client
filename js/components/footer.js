f3149e572d230e2e858187abdee559d4a0f79e5c
Weslo <wesley.rockholz@gmail.com>
2016-04-25 13:00:24 -0400

    update support links and icons

--- js/components/footer.js
+++ js/components/footer.js
@@ -16,7 +16,7 @@ var footer = {
                 ]),
                 m("li", [
                   m("a", {target: "_blank", href: "http://facebook.com/AdventureGuildGame"}, [
-                    m("span", {class: "fa fa-facebook fa-fw"}),
+                    m("span", {class: "fa fa-facebook-official fa-fw"}),
                     "/AdventureGuildGame"
                   ])
                 ]),
@@ -46,33 +46,27 @@ var footer = {
             m("div", [
               m("ul", [
                 m("li", [
-                  m("a", {target: "_blank", href: "http://twitter.com/AdvGuildGame"}, [
-                    m("span", {class: "fa fa-twitter fa-fw"}),
-                    "@AdvGuildGame"
+                  m("a", {target: "_blank", href: "#"}, [
+                    m("span", {class: "fa fa-sticky-note fa-fw"}),
+                    "Patch Notes"
                   ])
                 ]),
                 m("li", [
-                  m("a", {target: "_blank", href: "http://facebook.com/AdventureGuildGame"}, [
-                    m("span", {class: "fa fa-facebook fa-fw"}),
-                    "/AdventureGuildGame"
+                  m("a", {target: "_blank", href: "#"}, [
+                    m("span", {class: "fa fa-bug fa-fw"}),
+                    "Found a Bug?"
                   ])
                 ]),
                 m("li", [
-                  m("a", {target: "_blank", href: "http://twitch.tv/YesAndGames"}, [
-                    m("span", {class: "fa fa-twitch fa-fw"}),
-                    "/YesAndGames"
+                  m("a", {target: "_blank", href: "#"}, [
+                    m("span", {class: "fa fa-envelope fa-fw"}),
+                    "Contact Support"
                   ])
                 ]),
                 m("li", [
-                  m("a", {target: "_blank", href: "http://kickstarter.com/projects/yesandgames/adventure-guild"}, [
-                    m("span", {class: "fa fa-external-link fa-fw"}),
-                    "Kickstarter"
-                  ])
-                ]),
-                m("li", [
-                  m("a", {onclick: function(e){m.route("/press");}}, [
-                    m("span", {class: "fa fa-newspaper-o fa-fw"}),
-                    "Presskit"
+                  m("a", {target: "_blank", href: "#"}, [
+                    m("span", {class: "fa fa-lock fa-fw"}),
+                    "TOS and Privacy Policy"
                   ])
                 ]),
               ])
