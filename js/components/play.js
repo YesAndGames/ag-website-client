a487e731eb054cec802d1e266410375812776182
Weslo <wesley.rockholz@gmail.com>
2016-04-29 13:52:21 -0400

    add google play badge

--- js/components/play.js
+++ js/components/play.js
@@ -86,7 +86,8 @@ var alphaSignup = {
               ),
               m(".badge-container", [
                 m("a", m("img", {src: "imgs/badges/app-store.svg", alt: "Download on the App Store"})),
-                m("a", m("img", {src: "imgs/badges/app-store.svg", alt: "Download on the App Store"})),
+                m("a", {href: "https://play.google.com/store/apps/details?id=com.YesAndGames.AdventureGuild&utm_source=global_co&utm_medium=prtnr&utm_content=Mar2515&utm_campaign=PartBadge&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"},
+                  m("img", {src: "imgs/badges/google-play.png", alt: "Get it on Google Play"})),
               ])
             ])
           ),
