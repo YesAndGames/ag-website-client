3de35da2ea8a19a9dfa9803660a5e6e72d1dd475
Weslo <wesley.rockholz@gmail.com>
2016-05-06 14:09:25 -0400

    remove references to reddit for now, replace with twitter

--- js/components/modals.js
+++ js/components/modals.js
@@ -365,7 +365,8 @@ var contactUsModal = {
         m("hr", {class:"hr-gradient"}),
         m(".message-content", [
           m("p", "Our site is still in development, so we don't have resources on our site for sharing bugs and feedback. " +
-          "Reach out to us by posting on our subreddit, where our developers and users are regularly active and can respond and address your question or feedback as soon as possible!"),
+          "Reach out to us by tweeting at us for now. Our developers and users are regularly active and can respond and address your question or feedback as soon as possible!"),
+          // "Reach out to us by posting on our subreddit, where our developers and users are regularly active and can respond and address your question or feedback as soon as possible!"),
         ]),
         m("br"),
         m(".message-content", [
@@ -376,7 +377,8 @@ var contactUsModal = {
           ),
         ]),
         m("hr", {class:"hr-gradient"}),
-        m("a", {target: "_blank", href: "http://reddit.com/r/adventureguild"}, m("button", "/r/AdventureGuild")),
+        m("a", {href: "http://twitter.com/AdvGuildGame"}, m("button", "@AdvGuildGame")),
+        // m("a", {target: "_blank", href: "http://reddit.com/r/adventureguild"}, m("button", "/r/AdventureGuild")),
       ])
     );
   }
--- js/components/support.js
+++ js/components/support.js
@@ -15,8 +15,10 @@ var supportContactInfo = {
       m("hr", {class:"hr-gradient"}),
       m("div", [
         m("p", "Our site is still in development, so we don't have resources on our site for sharing bugs and feedback. " +
-        "Reach out to us by posting on our subreddit, where our developers and users are regularly active and can respond and address your question or feedback as soon as possible!"),
-        m(".reddit-button-container", m("a", {href: "http://reddit.com/r/adventureguild"}, m("button", "/r/adventureguild"))),
+        "Reach out to us by tweeting at us for now. Our developers and users are regularly active and can respond and address your question or feedback as soon as possible!"),
+        m(".reddit-button-container", m("a", {href: "http://twitter.com/AdvGuildGame"}, m("button", "@AdvGuildGame"))),
+        //"Reach out to us by posting on our subreddit, where our developers and users are regularly active and can respond and address your question or feedback as soon as possible!"),
+        //m(".reddit-button-container", m("a", {href: "http://reddit.com/r/adventureguild"}, m("button", "/r/adventureguild"))),
       ]),
       m("div", [
         m("p",
