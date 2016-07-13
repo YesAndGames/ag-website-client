989b538568d180b0b93aaa44461e6ae41ebb09ed
Weslo <wesley.rockholz@gmail.com>
2016-07-13 10:06:18 -0400

    add reddit social link and change all span icons to i

--- js/components/footer.js
+++ js/components/footer.js
@@ -10,31 +10,37 @@ var footer = {
               m("ul", [
                 m("li", [
                   m("a", {target: "_blank", href: "http://twitter.com/AdvGuildGame"}, [
-                    m("span", {class: "fa fa-twitter fa-fw"}),
+                    m("i", {class: "fa fa-twitter fa-fw"}),
                     "@AdvGuildGame"
                   ])
                 ]),
                 m("li", [
                   m("a", {target: "_blank", href: "http://facebook.com/AdventureGuildGame"}, [
-                    m("span", {class: "fa fa-facebook-official fa-fw"}),
+                    m("i", {class: "fa fa-facebook-official fa-fw"}),
                     "/AdventureGuildGame"
                   ])
                 ]),
                 m("li", [
+                  m("a", {target: "_blank", href: "http://reddit.com/r/adventureguild"}, [
+                    m("i", {class: "fa fa-reddit fa-fw"}),
+                    "/r/AdventureGuild"
+                  ])
+                ]),
+                m("li", [
                   m("a", {target: "_blank", href: "http://twitch.tv/YesAndGames"}, [
-                    m("span", {class: "fa fa-twitch fa-fw"}),
+                    m("i", {class: "fa fa-twitch fa-fw"}),
                     "/YesAndGames"
                   ])
                 ]),
                 m("li", [
                   m("a", {target: "_blank", href: "http://kickstarter.com/projects/yesandgames/adventure-guild"}, [
-                    m("span", {class: "fa fa-external-link fa-fw"}),
+                    m("i", {class: "fa fa-external-link fa-fw"}),
                     "Kickstarter"
                   ])
                 ]),
                 m("li", [
                   m("a", {onclick: function(e){m.route("/press");}}, [
-                    m("span", {class: "fa fa-newspaper-o fa-fw"}),
+                    m("i", {class: "fa fa-newspaper-o fa-fw"}),
                     "Presskit"
                   ])
                 ]),
@@ -47,25 +53,25 @@ var footer = {
               m("ul", [
                 m("li", [
                   m("a", {onclick: function(e) {m.route("/updates");}}, [
-                    m("span", {class: "fa fa-sticky-note fa-fw"}),
+                    m("i", {class: "fa fa-sticky-note fa-fw"}),
                     "Patch Notes"
                   ])
                 ]),
                 m("li", [
                   m("a", {onclick: function(e) {openModal(e, contactUsModal);}}, [
-                    m("span", {class: "fa fa-bug fa-fw"}),
+                    m("i", {class: "fa fa-bug fa-fw"}),
                     "Found a Bug?"
                   ])
                 ]),
                 m("li", [
                   m("a", {onclick: function(e) {openModal(e, contactUsModal);}}, [
-                    m("span", {class: "fa fa-envelope fa-fw"}),
+                    m("i", {class: "fa fa-envelope fa-fw"}),
                     "Contact Support"
                   ])
                 ]),
                 m("li", [
                   m("a", {onclick: function(e) {m.route("/privacy");}}, [
-                    m("span", {class: "fa fa-lock fa-fw"}),
+                    m("i", {class: "fa fa-lock fa-fw"}),
                     "Terms and Privacy"
                   ])
                 ]),
