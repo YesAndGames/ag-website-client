adb7cc07a415bde56ad5cb1222818b469e2a2d4a
RickNabb <nrabb@outlook.com>
2016-07-14 09:48:46 -0400

    Added global js file for the client to hold useful variables in. This resolves the yag_api_endpoint not defined issue

---
+++ js/client/global.js
@@ -0,0 +1,15 @@
+/**
+* global.js
+* A file to hold all of the gloabl scope variables and functions that are needed for the site.
+* Author: Nick Rabb <nrabb@outlook.com>
+* Yes And Games (C) 2016
+* yesandgames@gmail.com
+*/
+
+// VARIABLES
+// ================
+
+var endpoint_dev = 'localhost:8080/';
+var endpoint_prod = 'http://54.172.92.244:8080/';
+
+var yag_api_endpoint = endpoint_prod;
